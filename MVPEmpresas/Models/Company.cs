b0c998c0bcf351d3f6dbff575b17e2c8ba2718d9
Marcos <marcos.carranza1994@gmail.com>
2023-03-01 21:56:40 -0700

    Primer Commit

---
+++ MVPEmpresas/Models/Company.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel;
+
+namespace MVPEmpresas.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Clave")]
+        public string Key { get; set; }
+
+        [Required]
+        [DisplayName("Nombre")]
+        public string Name { get; set; }
+    
+        public string Alias { get; set; }
+        [Required]
+        [DisplayName("Direccion")]
+        public string Address { get; set; }
+        [Required]
+        [DisplayName("País")]
+        public string Country { get; set; }
+        [Required]
+        [DisplayName("Estado")]
+        public string State { get; set; }
+        [Required]
+        [DisplayName("Ciudad")]
+        public string City { get; set; }
+        [Required]
+        [DisplayName("Contacto")]
+        public string Contact { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DisplayName("Telefono")]
+        public string Telephone { get; set; }
+        [Required]
+        [DisplayName("Celular")]
+        public string Cellphone { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ MVPEmpresas/Models/EmpresasDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVPEmpresas.Models
+{
+    public class EmpresasDbContext:DbContext
+    {
+        public EmpresasDbContext(DbContextOptions<EmpresasDbContext> options)
+            : base( options) { }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
